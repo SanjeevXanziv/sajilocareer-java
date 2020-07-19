d2ce479f2e6207315c7e5708ca2847e097937215
lexitech99 <lexitech99@gmail.com>
2020-07-19 11:47:03 +0545

    First program and Variables

---
+++ VariablesPrac.java
@@ -0,0 +1,20 @@
+public class VariablesPrac {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+
+		int sum = a + b;
+
+		System.out.println(sum);
+
+		String name = "Shrishes";
+		String gfName = "Abc";
+
+		String mehendi = name + " " + gfName;
+
+		System.out.println(mehendi);
+
+		float bankBalance = 100.0f;
+		System.out.println(bankBalance);
+	}
+}
